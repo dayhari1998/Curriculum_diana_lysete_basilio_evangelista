54c5c474e72368eb115dc90c53ff0f82076bbb24
unknown <20183l301027@utcv.edu.mx>
2021-08-04 00:31:48 -0500

    se subio el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { InfoComponent } from './info/info.component';
+import { UnoComponent } from './uno/uno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    UnoComponent
   ],
+  // exports: [
+  //   HomeComponent
+  // ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
